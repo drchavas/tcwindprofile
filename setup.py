308d1b0413c8ba3b794b8bcd049cd8e71f4a3932
dchavas <drchavas@gmail.com>
2025-11-09 07:33:36 +0900

    minor edits to comments in python notebook and test plotting .py file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tcwindprofile',
-    version='2.0.0',
+    version='2.0.1',
     packages=find_packages(),
     install_requires=[],
     author='Dan Chavas',
